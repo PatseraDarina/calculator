2bb20fbf589714fb797e2137b5d10cfa6d3f3f38
PatseraDarina <darinapacera@gmail.com>
2018-07-17 23:20:25 +0300

    Task was completed

--- calculator/src/main/java/com/osdb/patsera/calculator/CalculatorApplication.java
+++ calculator/src/main/java/com/osdb/patsera/calculator/CalculatorApplication.java
@@ -2,8 +2,10 @@ package com.osdb.patsera.calculator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CalculatorApplication {
 
 	public static void main(String[] args) {
